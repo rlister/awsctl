b25bcc558a1fc3850275dd60a5a6ace2b1470fe6
Richard Lister <rlister@gmail.com>
2022-09-27 19:19:17 -0400

    whitespace

--- iam/cmd/get.go
+++ iam/cmd/get.go
@@ -6,9 +6,10 @@ import (
 
 // getCmd represents the get command
 var getCmd = &cobra.Command{
-	Use:   "get CMD",
-	Short: "Get iam resources",
-	Long: `Get detail for iam resources: roles, profiles.`,
+	Use:     "get CMD",
+	Aliases: []string{"g"},
+	Short:   "Get iam resources",
+	Long:    `Get detail for iam resources: roles, profiles.`,
 }
 
 func init() {
